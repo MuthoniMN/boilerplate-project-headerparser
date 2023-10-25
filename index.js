1525e85ae47d42616c94893fc3d4699a375378eb
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-10-25 12:39:48 +0300

    basic try

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  console.log(req.headers)
+  let obj = {
+    "ipadress": req.socket.remoteAddress,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  }
+  res.json(obj)
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
