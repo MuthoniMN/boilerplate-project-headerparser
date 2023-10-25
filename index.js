57135f5ce9eb4cb656c2925220d869ee34be8ddb
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-10-25 13:33:08 +0300

    trying request-ip

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 // init project
 require('dotenv').config();
 var express = require('express');
+var requestIp = require('request-ip')
 var app = express();
+
+app.use(requestIp.mw())
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -13,6 +16,8 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.set('trust proxy', true)
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
@@ -25,9 +30,8 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get('/api/whoami', function (req, res) {
-  console.log(req.headers)
   let obj = {
-    "ipadress": req.socket.remoteAddress,
+    "ipadress": req.clientIp,
     "language": req.headers['accept-language'],
     "software": req.headers['user-agent']
   }
